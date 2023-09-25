f33bb5351586ce173df3d83ed21d4970527fedeb
Marcos Kaiô <mkaio.sebrae@gmail.com>
2023-09-25 15:29:27 -0300

    iniciando funcionalidades principais do game

---
+++ guess-a-mon/main.js
@@ -0,0 +1,41 @@
+function getRandomPokemon() {
+    fetch('https://pokeapi.co/api/v2/pokemon?limit=1')
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            const maxPokemonCount = data.count;
+            const randomPokemonId = Math.floor(Math.random() * maxPokemonCount) + 1;
+            
+            fetch(`https://pokeapi.co/api/v2/pokemon/${randomPokemonId}`)
+                .then(function (response) {
+                    return response.json();
+                })
+                .then(function (response) {
+                    console.log(response);
+                    const pokebox = document.getElementById('pokebox');
+                    pokebox.style.display = 'flex';
+
+                    document.getElementById('poke-picture').src = response.sprites.front_default;
+
+                    currentPokemonName = response.name;
+                });
+        });
+
+    const botao = document.getElementById('startbutton')
+    const guessform = document.getElementById('guessform')
+    botao.style.display = 'none'
+    guessform.style.display = 'flex'
+
+}
+
+function check(){
+    const guessBox = document.getElementById('guessbox');
+    const userGuess = guessBox.value.toLowerCase();
+
+    if (userGuess === currentPokemonName) {
+        alert('Você acertou!');
+    } else {
+        alert('Errado!'); 
+    }
+}
