090244d58eda73b10d914b339f43de1f23bdbb42
Marcos Kaiô <mkaio.sebrae@gmail.com>
2023-09-27 18:11:08 -0300

    Jogo funcionando corretamente com sistema de pontos

--- guess-a-mon/main.js
+++ guess-a-mon/main.js
@@ -2,13 +2,15 @@ function getRandomInt(max) {
     return Math.floor(Math.random() * max);
 };
 
+let pontos = 0
+
 function getRandomPokemon() {
     fetch('https://pokeapi.co/api/v2/pokemon?limit=1')
         .then(function (response) {
             return response.json();
         })
         .then(function (data) {
-            const maxPokemonCount = 1017;
+            const maxPokemonCount = 1018;
             const randomPokemonId = getRandomInt(maxPokemonCount)
 
             fetch(`https://pokeapi.co/api/v2/pokemon/${randomPokemonId}`)
@@ -38,17 +40,43 @@ function getRandomPokemon() {
 
 }
 
+function keep() {
+    const maxPokemonCount = 1018;
+    let randomPokemonId = getRandomInt(maxPokemonCount)
+    fetch(`https://pokeapi.co/api/v2/pokemon/${randomPokemonId}`)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (response) {
+            console.log(response);
+            document.getElementById('poke-picture').src = response.sprites.front_default
+
+            currentPokemonName =  response.name
+        })
+        .catch(function (error) {
+            console.error('Ocorreu um erro:', error);
+        });
+}
+
 
 function check() {
     const guessBox = document.getElementById('guessbox');
     const userGuess = guessBox.value.toLowerCase();
 
-
     if (userGuess === currentPokemonName) {
         alert("Você acertou")
+        pontos=pontos+3
+        keep()
     } else {
-        alert("Errado! Tente novamente")
+        if(pontos>0){
+            alert("Errado! Tente novamente. Nome correto: "+currentPokemonName)
+            pontos = pontos-1 
+            keep()
+        } else{
+            alert("Errado! Fim de jogo. Tente novamente. Nome correto: "+currentPokemonName)
+            location.reload()
+        }
+        
     }
+    document.getElementById('points').innerHTML = "Pontos: "+pontos
 }
-
-
